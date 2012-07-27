be833ecd7d43e4f8147960f28719dc3090a7ef67
George Shakhnazaryan <gshakhn@gmail.com>
2012-07-26 23:23:23 -0500

    Added basic support for lexing tables.

--- src/main/jflex/com/gshakhn/idea/idea/fitnesse/lang/lexer/Fitnesse.flex
+++ src/main/jflex/com/gshakhn/idea/idea/fitnesse/lang/lexer/Fitnesse.flex
@@ -16,8 +16,16 @@ import com.intellij.psi.tree.IElementType;
 LINE_TERMINATOR = \n|\r\n
 WIKI_WORD =       ([A-Z][a-z0-9]+)+([A-Z][a-z0-9]+)
 
+%state TABLE
+
 %%
 
 <YYINITIAL> {LINE_TERMINATOR}  {return FitnesseElementType.LINE_TERMINATOR();}
 <YYINITIAL> {WIKI_WORD}        {return FitnesseElementType.WIKI_WORD();}
+<YYINITIAL> \|                 {yybegin(TABLE);return FitnesseElementType.CELL_DELIM();}
 <YYINITIAL> .                  {return FitnesseElementType.REGULAR_TEXT();}
+
+<TABLE>     \|                                  {return FitnesseElementType.CELL_DELIM();}
+<TABLE>     {LINE_TERMINATOR}{LINE_TERMINATOR}  {yybegin(YYINITIAL);return FitnesseElementType.TABLE_END();}
+<TABLE>     {LINE_TERMINATOR}                   {yybegin(YYINITIAL);return FitnesseElementType.ROW_END();}
+<TABLE>     [^\r\n\|]+                          {yybegin(YYINITIAL);return FitnesseElementType.CELL_TEXT();}
---
+++ src/test/scala/com/gshakhn/idea/idea/fitnesse/lang/lexer/TableSuite.scala
@@ -0,0 +1,29 @@
+package com.gshakhn.idea.idea.fitnesse.lang.lexer
+
+class TableSuite extends LexerSuite {
+  test("Simple table") {
+    expect(
+      List(
+        (FitnesseElementType.CELL_DELIM, "|"),
+        (FitnesseElementType.CELL_TEXT, "header1"),
+        (FitnesseElementType.CELL_DELIM, "|"),
+        (FitnesseElementType.CELL_TEXT, "header2"),
+        (FitnesseElementType.CELL_DELIM, "|"),
+        (FitnesseElementType.ROW_END, "\n"),
+        (FitnesseElementType.CELL_DELIM, "|"),
+        (FitnesseElementType.CELL_TEXT, "row1 col1"),
+        (FitnesseElementType.CELL_DELIM, "|"),
+        (FitnesseElementType.CELL_TEXT, "row1 col2"),
+        (FitnesseElementType.CELL_DELIM, "|"),
+        (FitnesseElementType.ROW_END, "\n"),
+        (FitnesseElementType.CELL_DELIM, "|"),
+        (FitnesseElementType.CELL_TEXT, "row2 col1"),
+        (FitnesseElementType.CELL_DELIM, "|"),
+        (FitnesseElementType.CELL_TEXT, "row2 col2"),
+        (FitnesseElementType.CELL_DELIM, "|"),
+        (FitnesseElementType.TABLE_END, "\n\n")
+      )) {
+      lex("|header1|header2|\n|row1 col1|row1 col2|\n|row2 col1|row2 col2|\n\n")
+    }
+  }
+}
