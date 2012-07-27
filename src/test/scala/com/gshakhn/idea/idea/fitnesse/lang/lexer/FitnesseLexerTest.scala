a7bd2cbc0b5ba1e5bf76c035dc2f61f5544723cd
George Shakhnazaryan <gshakhn@gmail.com>
2012-07-26 21:45:51 -0500

    Adding test that mixes a WikiWord with regular text.

--- src/test/scala/com/gshakhn/idea/idea/fitnesse/lang/lexer/FitnesseLexerTest.scala
+++ src/test/scala/com/gshakhn/idea/idea/fitnesse/lang/lexer/FitnesseLexerTest.scala
@@ -17,6 +17,27 @@ class FitnesseLexerTest extends FunSuite {
     doTest("LongerWikiWord", List(("Fitnesse:WIKI_WORD", "LongerWikiWord")))
   }
 
+  test("Regular text followed by Wiki Word") {
+    doTest("Some text and WikiWord",
+      List(
+        ("Fitnesse:REGULAR_TEXT", "S"),
+        ("Fitnesse:REGULAR_TEXT", "o"),
+        ("Fitnesse:REGULAR_TEXT", "m"),
+        ("Fitnesse:REGULAR_TEXT", "e"),
+        ("Fitnesse:REGULAR_TEXT", " "),
+        ("Fitnesse:REGULAR_TEXT", "t"),
+        ("Fitnesse:REGULAR_TEXT", "e"),
+        ("Fitnesse:REGULAR_TEXT", "x"),
+        ("Fitnesse:REGULAR_TEXT", "t"),
+        ("Fitnesse:REGULAR_TEXT", " "),
+        ("Fitnesse:REGULAR_TEXT", "a"),
+        ("Fitnesse:REGULAR_TEXT", "n"),
+        ("Fitnesse:REGULAR_TEXT", "d"),
+        ("Fitnesse:REGULAR_TEXT", " "),
+        ("Fitnesse:WIKI_WORD", "WikiWord")
+      ))
+  }
+
   test("Single word") {
     doTest("Hello",
       List(
