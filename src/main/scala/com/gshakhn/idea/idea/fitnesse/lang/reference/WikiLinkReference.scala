3e73597972900b2c5e26385c16c289ae6408c4c0
George Shakhnazaryan <gshakhn@gmail.com>
2012-09-29 12:32:33 -0500

    Extract getParentDir method from resolve to make code slightly more readable.

--- src/main/scala/com/gshakhn/idea/idea/fitnesse/lang/reference/WikiLinkReference.scala
+++ src/main/scala/com/gshakhn/idea/idea/fitnesse/lang/reference/WikiLinkReference.scala
@@ -14,26 +14,7 @@ class WikiLinkReference(element: WikiLink) extends PsiReferenceBase[WikiLink](el
       case _:WikiLinkElementType => element.getText.substring(1)
     }
     var dirs = linkText.split('.')
-    val parentDir: PsiDirectory = element.getNode.getElementType match {
-      case WikiLinkElementType.RELATIVE_WIKI_LINK => element.getContainingFile.getParent.getParent // go up twice since the immediate parent only contains content.txt and properties.xml
-      case WikiLinkElementType.SUBPAGE_WIKI_LINK => element.getContainingFile.getParent
-      case WikiLinkElementType.ABSOLUTE_WIKI_LINK => {
-        var currentFolder = element.getContainingFile.getParent
-        while (FileTypeIndex.getFiles(FitnesseFileType.INSTANCE, GlobalSearchScopes.directoryScope(currentFolder.getParent, false)).size() > 0) {
-          currentFolder = currentFolder.getParent
-        }
-        currentFolder
-      }
-      case WikiLinkElementType.ANCESTOR_WIKI_LINK => {
-        var currentFolder = element.getContainingFile.getParent
-        while (!currentFolder.getName.equalsIgnoreCase(dirs(0))
-              && FileTypeIndex.getFiles(FitnesseFileType.INSTANCE, GlobalSearchScopes.directoryScope(currentFolder.getParent, false)).size() > 0) {
-          currentFolder = currentFolder.getParent
-        }
-        dirs = dirs.tail
-        currentFolder
-      }
-    }
+    val parentDir: PsiDirectory = getParentDir(dirs)
     val targetDir = dirs.foldLeft(parentDir) {
         (currentDir: PsiDirectory, childName: String) => {
           currentDir match {
@@ -49,6 +30,31 @@ class WikiLinkReference(element: WikiLink) extends PsiReferenceBase[WikiLink](el
     val targetFiles = FileTypeIndex.getFiles(FitnesseFileType.INSTANCE, GlobalSearchScopes.directoryScope(targetDir, false))
 
     PsiManager.getInstance(element.getProject).findFile(targetFiles.head)
+  }
+
+
+  private def getParentDir(_dirs: Array[String]): PsiDirectory = {
+    var dirs: Array[String] = _dirs
+    element.getNode.getElementType match {
+      case WikiLinkElementType.RELATIVE_WIKI_LINK => element.getContainingFile.getParent.getParent // go up twice since the immediate parent only contains content.txt and properties.xml
+      case WikiLinkElementType.SUBPAGE_WIKI_LINK => element.getContainingFile.getParent
+      case WikiLinkElementType.ABSOLUTE_WIKI_LINK => {
+        var currentFolder = element.getContainingFile.getParent
+        while (FileTypeIndex.getFiles(FitnesseFileType.INSTANCE, GlobalSearchScopes.directoryScope(currentFolder.getParent, false)).size() > 0) {
+          currentFolder = currentFolder.getParent
+        }
+        currentFolder
+      }
+      case WikiLinkElementType.ANCESTOR_WIKI_LINK => {
+        var currentFolder = element.getContainingFile.getParent
+        while (!currentFolder.getName.equalsIgnoreCase(dirs(0))
+            && FileTypeIndex.getFiles(FitnesseFileType.INSTANCE, GlobalSearchScopes.directoryScope(currentFolder.getParent, false)).size() > 0) {
+          currentFolder = currentFolder.getParent
+        }
+        dirs = dirs.tail
+        currentFolder
+      }
+    }
   }
 
   def getVariants = Array()
