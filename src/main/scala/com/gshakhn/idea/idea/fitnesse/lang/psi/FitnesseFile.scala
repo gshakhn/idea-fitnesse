4895b72e81d26c08320bf5226e29b014d38b19fd
George Shakhnazaryan <gshakhn@gmail.com>
2012-09-30 12:15:46 -0500

    Fix FitnesseFile.getName to work when the virtual file is a LightVirtualFile.
    IntelliJ occasionally gives the LightVirtualFile instead of the real file, so we have to go to the original.

--- src/main/scala/com/gshakhn/idea/idea/fitnesse/lang/psi/FitnesseFile.scala
+++ src/main/scala/com/gshakhn/idea/idea/fitnesse/lang/psi/FitnesseFile.scala
@@ -1,12 +1,16 @@
 package com.gshakhn.idea.idea.fitnesse.lang.psi
 
 import com.intellij.extapi.psi.PsiFileBase
-import com.intellij.psi.FileViewProvider
+import com.intellij.psi.{PsiDirectory, FileViewProvider}
 import com.gshakhn.idea.idea.fitnesse.lang.FitnesseLanguage
 import com.gshakhn.idea.idea.fitnesse.lang.filetype.FitnesseFileType
+import com.intellij.testFramework.LightVirtualFile
 
 class FitnesseFile(fileViewProvider: FileViewProvider) extends PsiFileBase(fileViewProvider, FitnesseLanguage.INSTANCE) {
   def getFileType = FitnesseFileType.INSTANCE
 
-  override def getName = getContainingDirectory.getName
+  override def getName = getViewProvider.getVirtualFile match {
+    case lvf: LightVirtualFile => lvf.getOriginalFile.getParent.getName
+    case vf => vf.getParent.getName
+  }
 }
