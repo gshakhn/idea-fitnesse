887fdc23e075cde38d6d7a7ef346a72a9b025a1f
George Shakhnazaryan <gshakhn@gmail.com>
2012-09-30 10:51:28 -0500

    Make decision table parser test test something that looks like a decision table.

--- src/test/scala/com/gshakhn/idea/idea/fitnesse/lang/parser/TableParserSuite.scala
+++ src/test/scala/com/gshakhn/idea/idea/fitnesse/lang/parser/TableParserSuite.scala
@@ -16,11 +16,16 @@ class TableParserSuite extends ParserSuite {
             Leaf(FitnesseTokenType.CELL_DELIM, "|"),
             Leaf(FitnesseTokenType.CELL_TEXT, "B"),
             Leaf(FitnesseTokenType.CELL_DELIM, "|")
+          )),
+          Node(FitnesseElementType.ROW, List(
+            Leaf(FitnesseTokenType.CELL_DELIM, "|"),
+            Leaf(FitnesseTokenType.CELL_TEXT, "C"),
+            Leaf(FitnesseTokenType.CELL_DELIM, "|")
           ))
         ))
       ))
     ) {
-      parse("|A|\n|B|\n\n")
+      parse("|A|\n|B|\n|C|\n\n")
     }
   }
 
@@ -30,18 +35,27 @@ class TableParserSuite extends ParserSuite {
         Node(TableElementType.DECISION_TABLE, List(
           Node(FitnesseElementType.ROW, List(
             Leaf(FitnesseTokenType.CELL_DELIM, "|"),
-            Leaf(FitnesseTokenType.CELL_TEXT, "Hello World"),
+            Leaf(FitnesseTokenType.CELL_TEXT, "Should I buy it"),
             Leaf(FitnesseTokenType.CELL_DELIM, "|")
           )),
           Node(FitnesseElementType.ROW, List(
             Leaf(FitnesseTokenType.CELL_DELIM, "|"),
-            Leaf(FitnesseTokenType.CELL_TEXT, "goodbye world"),
+            Leaf(FitnesseTokenType.CELL_TEXT, "have money"),
+            Leaf(FitnesseTokenType.CELL_DELIM, "|"),
+            Leaf(FitnesseTokenType.CELL_TEXT, "buy it?"),
+            Leaf(FitnesseTokenType.CELL_DELIM, "|")
+          )),
+          Node(FitnesseElementType.ROW, List(
+            Leaf(FitnesseTokenType.CELL_DELIM, "|"),
+            Leaf(FitnesseTokenType.CELL_TEXT, "yes"),
+            Leaf(FitnesseTokenType.CELL_DELIM, "|"),
+            Leaf(FitnesseTokenType.CELL_TEXT, "yes"),
             Leaf(FitnesseTokenType.CELL_DELIM, "|")
           ))
         ))
       ))
     ) {
-      parse("|Hello World|\n|goodbye world|\n\n")
+      parse("|Should I buy it|\n|have money|buy it?|\n|yes|yes|\n\n")
     }
   }
 }
