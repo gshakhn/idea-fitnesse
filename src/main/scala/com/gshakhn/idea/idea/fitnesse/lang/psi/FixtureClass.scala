142cbe10c32d25f0efc2fef7a18a07c37ebcd0e7
Arjan Molenaar <gaphor@gmail.com>
2015-04-25 12:00:21 +0200

    Upgrade FitNesse 20150424.

--- src/main/scala/com/gshakhn/idea/idea/fitnesse/lang/psi/FixtureClass.scala
+++ src/main/scala/com/gshakhn/idea/idea/fitnesse/lang/psi/FixtureClass.scala
@@ -3,10 +3,11 @@ package com.gshakhn.idea.idea.fitnesse.lang.psi
 import com.gshakhn.idea.idea.fitnesse.lang.reference.FixtureClassReference
 import com.intellij.lang.ASTNode
 import com.intellij.psi.search.{GlobalSearchScope, PsiShortNamesCache}
+import fitnesse.testsystems.slim.tables.Disgracer.disgraceClassName
 
 class FixtureClass(node: ASTNode) extends Cell(node) {
 
-  def fixtureClassName = Disgracer.disgraceClassName(node.getText)
+  def fixtureClassName = disgraceClassName(node.getText)
 
   override def getReferences = {
     PsiShortNamesCache.getInstance(getProject)
