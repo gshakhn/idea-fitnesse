846888faaf766b2545b49e38d912d7ba54e5add8
Arjan Molenaar <gaphor@gmail.com>
2015-05-30 22:56:02 +0200

    fix decision table lookups.

--- src/main/scala/fitnesse/idea/decisiontable/DecisionInput.scala
+++ src/main/scala/fitnesse/idea/decisiontable/DecisionInput.scala
@@ -7,7 +7,7 @@ import fitnesse.testsystems.slim.tables.Disgracer.disgraceClassName
 
 class DecisionInput(node: ASTNode) extends Cell(node) with MethodReferences {
 
-  override def fixtureMethodName = "set" + disgraceClassName(node.getText)
+  override def fixtureMethodName = "set" + disgraceClassName(node.getText.trim.capitalize)
 
   override def createReference(psiMethod: PsiMethod) = new DecisionInputReference(psiMethod, this)
 }
