d1c58d472ed686e544808f92f82b9ac617680999
Arjan Molenaar <gaphor@gmail.com>
2015-06-01 22:24:19 +0200

    fix script row handling

--- src/main/scala/fitnesse/idea/scripttable/ScriptRow.scala
+++ src/main/scala/fitnesse/idea/scripttable/ScriptRow.scala
@@ -3,6 +3,7 @@ package fitnesse.idea.scripttable
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 import fitnesse.idea.lang.lexer.FitnesseTokenType
+import fitnesse.idea.lang.parser.FitnesseElementType
 import fitnesse.idea.lang.psi.{FixtureClass, MethodReferences, Row}
 import fitnesse.testsystems.slim.tables.Disgracer._
 
@@ -13,7 +14,7 @@ class ScriptRow(node: ASTNode) extends Row(node) with MethodReferences {
   override def getFixtureClass: Option[FixtureClass] = getTable.getFixtureClass
 
   override def fixtureMethodName: String = {
-    val snippets: java.util.List[PsiElement] = findChildrenByType(FitnesseTokenType.CELL_TEXT)
+    val snippets: java.util.List[PsiElement] = findChildrenByType(FitnesseElementType.CELL)
     val texts = snippets.map(_.getText.trim).toList
 
     def constructFixtureName(parts: List[String]) = {
@@ -24,6 +25,7 @@ class ScriptRow(node: ASTNode) extends Row(node) with MethodReferences {
       else
         parts.view.zipWithIndex.filter(_._2 % 2 == 0).map(_._1).fold("") { (a, b) =>
           a + (if (a == "") disgraceMethodName(b) else disgraceClassName(b.capitalize))
+          // TODO: make a string first, then disgrace it, like in the real code
         }
     }
 
