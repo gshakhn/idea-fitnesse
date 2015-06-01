48cac84499fbef3b0e731c463d07a271262eab26
Arjan Molenaar <gaphor@gmail.com>
2015-06-01 22:13:28 +0200

    Make disgrace method name more real

--- src/main/scala/fitnesse/idea/decisiontable/DecisionInput.scala
+++ src/main/scala/fitnesse/idea/decisiontable/DecisionInput.scala
@@ -3,11 +3,11 @@ package fitnesse.idea.decisiontable
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiMethod
 import fitnesse.idea.lang.psi.{Cell, MethodReferences}
-import fitnesse.testsystems.slim.tables.Disgracer.disgraceClassName
+import fitnesse.testsystems.slim.tables.Disgracer.disgraceMethodName
 
 class DecisionInput(node: ASTNode) extends Cell(node) with MethodReferences {
 
-  override def fixtureMethodName = "set" + disgraceClassName(node.getText.trim.capitalize)
+  override def fixtureMethodName = disgraceMethodName("set " + node.getText.trim)
 
   override def createReference(psiMethod: PsiMethod) = new DecisionInputReference(psiMethod, this)
 }
