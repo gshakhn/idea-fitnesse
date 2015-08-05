282066ea3d33839cc2338edeaef18a71d3684847
Arjan Molenaar <gaphor@gmail.com>
2015-08-05 22:58:50 +0200

    Fix issue in scenario references.

--- src/main/scala/fitnesse/idea/fixturemethod/MethodReferences.scala
+++ src/main/scala/fitnesse/idea/fixturemethod/MethodReferences.scala
@@ -3,6 +3,7 @@ package fitnesse.idea.fixturemethod
 import com.intellij.psi.search.{GlobalSearchScope, PsiShortNamesCache}
 import com.intellij.psi.{PsiClass, PsiElement, PsiMethod, PsiReference}
 import fitnesse.idea.fixtureclass.FixtureClass
+import fitnesse.idea.scripttable.ScenarioName
 
 trait MethodReferences extends PsiElement {
 
@@ -12,11 +13,16 @@ trait MethodReferences extends PsiElement {
 
   protected def createReference(psiMethod: PsiMethod): MethodReference = new MethodReference(psiMethod, this)
 
+  protected def createReference(scenarioName: ScenarioName): ScenarioReference = new ScenarioReference(scenarioName, this)
+
   protected def getReferencedMethods: Seq[PsiReference] = {
     getFixtureClass match {
       case Some(fixtureClass) =>
         fixtureClass.getReferences
-          .flatMap(_.resolve.asInstanceOf[PsiClass].findMethodsByName(fixtureMethodName, true /* checkBases */)).map(createReference)
+          .flatMap(_.resolve match {
+            case c : PsiClass => c.findMethodsByName(fixtureMethodName, true /* checkBases */).map(createReference)
+            case s : ScenarioName => List(createReference(s))
+          })
       case None =>
         val cache = PsiShortNamesCache.getInstance(getProject)
         cache.getMethodsByName(fixtureMethodName, GlobalSearchScope.projectScope(getProject)).map(createReference)
