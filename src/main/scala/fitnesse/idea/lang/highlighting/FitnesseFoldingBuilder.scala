49f19785500742f56c81b6c887a70fb02efbb2bc
Arjan Molenaar <gaphor@gmail.com>
2015-08-06 23:02:57 +0200

    Hopefully fix exceptions in folding builder.

--- src/main/scala/fitnesse/idea/lang/highlighting/FitnesseFoldingBuilder.scala
+++ src/main/scala/fitnesse/idea/lang/highlighting/FitnesseFoldingBuilder.scala
@@ -11,32 +11,31 @@ import fitnesse.idea.lang.parser.FitnesseElementType
 import fitnesse.idea.lang.psi.FitnesseFile
 
 class FitnesseFoldingBuilder extends FoldingBuilderEx {
-  var descriptors = List[FoldingDescriptor]()
 
   override def isCollapsedByDefault(astNode: ASTNode): Boolean = true
 
   override def buildFoldRegions(psiElement: PsiElement, document: Document, quick: Boolean): Array[FoldingDescriptor] = {
     psiElement match {
-      case g2: FitnesseFile => {
-        if (!quick) {
-          PsiTreeUtil.processElements(new FitnesseFileElementprocessor, g2)
-        }
-        descriptors.toArray
-      }
+      case g2: FitnesseFile if !quick =>
+        val processor: FitnesseFileElementprocessor = new FitnesseFileElementprocessor
+        PsiTreeUtil.processElements(processor, g2)
+        processor.descriptors.toArray
       case _ => FoldingDescriptor.EMPTY
     }
   }
 
   override def getPlaceholderText(astNode: ASTNode): String =
     astNode.getElementType match {
-      case FitnesseElementType.COLLAPSIBLE => {
+      case FitnesseElementType.COLLAPSIBLE =>
         val toList: List[String] = astNode.getText.split("\n").toList
         toList.head + " ... " + toList.last
-      }
       case _ => null
     }
 
   private class FitnesseFileElementprocessor extends PsiElementProcessor[PsiElement] {
+
+    var descriptors = List[FoldingDescriptor]()
+
     override def execute(t: PsiElement): Boolean = {
       if (t.getNode.getElementType == FitnesseElementType.COLLAPSIBLE) {
         descriptors = new FoldingDescriptor(t, t.getTextRange) :: descriptors
