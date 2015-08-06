15ed1a37cd52d12eb0cd5f8163ebd4f36e94b2e1
Arjan Molenaar <gaphor@gmail.com>
2015-08-06 23:05:35 +0200

    Simplify wiki link logic.

--- src/main/scala/fitnesse/idea/lang/parser/FitnesseElementType.scala
+++ src/main/scala/fitnesse/idea/lang/parser/FitnesseElementType.scala
@@ -44,14 +44,3 @@ object TableElementType {
   final val DEFINE_TABLE_TYPE_TABLE = new TableElementType("DEFINE_TABLE_TYPE_TABLE")
   final val DEFINE_ALIAS_TABLE = new TableElementType("DEFINE_ALIAS_TABLE")
 }
-
-class WikiLinkElementType(debugName: String) extends FitnesseElementType(debugName) {
-
-}
-
-object WikiLinkElementType {
-  final val RELATIVE_WIKI_LINK = new WikiLinkElementType("RELATIVE_WIKI_LINK")
-  final val ABSOLUTE_WIKI_LINK = new WikiLinkElementType("ABSOLUTE_WIKI_LINK")
-  final val SUBPAGE_WIKI_LINK = new WikiLinkElementType("SUPPAGE_WIKI_LINK")
-  final val ANCESTOR_WIKI_LINK = new WikiLinkElementType("ANCESTOR_WIKI_LINK")
-}
--- src/main/scala/fitnesse/idea/wikilink/WikiLinkReference.scala
+++ src/main/scala/fitnesse/idea/wikilink/WikiLinkReference.scala
@@ -3,16 +3,13 @@ package fitnesse.idea.wikilink
 import com.intellij.psi.search.{FileTypeIndex, GlobalSearchScopes}
 import com.intellij.psi.{PsiDirectory, PsiElement, PsiManager, PsiReferenceBase}
 import fitnesse.idea.lang.filetype.FitnesseFileType
-import fitnesse.idea.lang.parser.WikiLinkElementType
 
 import scala.collection.JavaConversions._
 
 class WikiLinkReference(element: WikiLink) extends PsiReferenceBase[WikiLink](element) {
   override def resolve() : PsiElement = {
-    val linkText = element.getNode.getElementType match {
-      case WikiLinkElementType.RELATIVE_WIKI_LINK => element.getText
-      case _:WikiLinkElementType => element.getText.substring(1)
-    }
+    val linkText = element.getText
+
     val dirs = linkText.split('.')
     val parentDir: PsiDirectory = getParentDir(dirs)
     val targetDir = dirs.foldLeft(parentDir) {
@@ -27,34 +24,12 @@ class WikiLinkReference(element: WikiLink) extends PsiReferenceBase[WikiLink](el
     if (targetDir == null) {
       return null
     }
-    val targetFiles = FileTypeIndex.getFiles(FitnesseFileType.INSTANCE, GlobalSearchScopes.openFilesScope(element.getProject))
 
-    PsiManager.getInstance(element.getProject).findFile(targetFiles.head)
+    targetDir.findFile("content.txt")
   }
 
-
-  private def getParentDir(_dirs: Array[String]): PsiDirectory = {
-    var dirs: Array[String] = _dirs
-    element.getNode.getElementType match {
-      case WikiLinkElementType.RELATIVE_WIKI_LINK => element.getContainingFile.getParent.getParent // go up twice since the immediate parent only contains content.txt and properties.xml
-      case WikiLinkElementType.SUBPAGE_WIKI_LINK => element.getContainingFile.getParent
-      case WikiLinkElementType.ABSOLUTE_WIKI_LINK => {
-        var currentFolder = element.getContainingFile.getParent
-        while (FileTypeIndex.getFiles(FitnesseFileType.INSTANCE, GlobalSearchScopes.openFilesScope(element.getProject)).size() > 0) {
-          currentFolder = currentFolder.getParent
-        }
-        currentFolder
-      }
-      case WikiLinkElementType.ANCESTOR_WIKI_LINK => {
-        var currentFolder = element.getContainingFile.getParent
-        while (!currentFolder.getName.equalsIgnoreCase(dirs(0))
-            && FileTypeIndex.getFiles(FitnesseFileType.INSTANCE, GlobalSearchScopes.openFilesScope(element.getProject)).size() > 0) {
-          currentFolder = currentFolder.getParent
-        }
-        dirs = dirs.tail
-        currentFolder
-      }
-    }
+  private def getParentDir(pagePath: Array[String]): PsiDirectory = {
+    element.getContainingFile.getParent.getParent // go up twice since the immediate parent only contains content.txt and properties.xml
   }
 
   override def getVariants = Array()
