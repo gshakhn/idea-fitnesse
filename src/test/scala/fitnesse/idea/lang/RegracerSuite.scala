f861e6ea7ec20811123520eb96fee200ae2fec10
Arjan Molenaar <gaphor@gmail.com>
2015-08-30 12:09:12 +0200

    Fix regracer for single-character words

--- src/test/scala/fitnesse/idea/lang/RegracerSuite.scala
+++ src/test/scala/fitnesse/idea/lang/RegracerSuite.scala
@@ -1,5 +1,6 @@
 package fitnesse.idea.lang
 
+import fitnesse.testsystems.slim.tables.Disgracer
 import org.scalatest.FunSuite
 
 class RegracerSuite extends FunSuite {
@@ -21,5 +22,14 @@ class RegracerSuite extends FunSuite {
       Regracer.regrace("methodName")
     }
   }
+
+  test("method name regrace with single character words") {
+    val s = "I am a coder"
+    assertResult(s.toLowerCase) {
+      Regracer.regrace(Disgracer.disgraceClassName(s))
+    }
+    assertResult(s.toLowerCase) {
+      Regracer.regrace(Disgracer.disgraceMethodName(s))
+    }  }
 
 }
