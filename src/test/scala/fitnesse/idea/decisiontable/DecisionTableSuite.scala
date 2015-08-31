d275b65114d27c5f3884b630a55389c975f2b595
Arjan Molenaar <gaphor@gmail.com>
2015-08-31 09:35:55 +0200

    boyscout decision table tests

--- src/test/scala/fitnesse/idea/decisiontable/DecisionTableSuite.scala
+++ src/test/scala/fitnesse/idea/decisiontable/DecisionTableSuite.scala
@@ -2,17 +2,14 @@ package fitnesse.idea.decisiontable
 
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.stubs.{StubBase, StubElement}
+import com.intellij.psi.stubs.StubBase
 import com.intellij.psi.{ElementManipulators, PsiClass, PsiMethod}
-import fitnesse.idea.lang.FitnesseLanguage
-import fitnesse.idea.lang.psi.{FitnesseFile, PsiSuite, Table}
-import fitnesse.idea.scripttable.{ScenarioNameStubImpl, ScenarioNameImpl, ScenarioName, ScenarioNameIndex}
+import fitnesse.idea.lang.psi.{FitnesseElementFactory, FitnesseFile, PsiSuite, Table}
+import fitnesse.idea.scripttable._
 import org.mockito.Matchers.{any, anyBoolean, eq => m_eq}
 import org.mockito.Mockito.when
-import org.mockito.stubbing.OngoingStubbing
 
 import scala.collection.JavaConverters._
-import scala.collection.JavaConversions._
 
 class DecisionTableSuite extends PsiSuite {
 
@@ -40,7 +37,7 @@ class DecisionTableSuite extends PsiSuite {
   }
 
   def decisionTable(s: String): Table = {
-    val psiFile = myPsiFileFactory.createFileFromText(FitnesseLanguage.INSTANCE, s)
+    val psiFile = FitnesseElementFactory.createFile(myProject, s)
     psiFile.getNode.getPsi(classOf[FitnesseFile]).getTables(0)
   }
 
@@ -90,7 +87,7 @@ class DecisionTableSuite extends PsiSuite {
     val myScenarioCallMe: ScenarioName = new ScenarioNameImpl(new ScenarioNameStubImpl(mock[StubBase[Table]], "callMe", List("arg1", "arg2")))
     val output = decisionTable("| call me |").getFixtureClass.get
     when(myStubIndex.get(m_eq(ScenarioNameIndex.KEY), m_eq("CallMe"), any[Project], any[GlobalSearchScope])).thenReturn(List(myScenarioCallMe).asJava)
-    bypassShortNameCache
+    bypassShortNameCache()
     assertResult(myScenarioCallMe) {
       val refs = output.getReferences
       refs(0).resolve
@@ -99,10 +96,10 @@ class DecisionTableSuite extends PsiSuite {
 
 
   test("scenario arguments as completion options") {
-    val myScenarioCallMe: ScenarioName = new ScenarioNameImpl(new ScenarioNameStubImpl(mock[StubBase[Table]], "decision table", List("arg1", "arg2")))
+    val myScenarioCallMe: ScenarioName = ScenarioNameElementTypeHolder.INSTANCE.createPsi(new ScenarioNameStubImpl(mock[StubBase[Table]], "decision table", List("arg1", "arg2")))
     val decisionInput = table.getRows(1).getCells(0)
     when(myStubIndex.get(m_eq(ScenarioNameIndex.KEY), m_eq("DecisionTable"), any[Project], any[GlobalSearchScope])).thenReturn(List(myScenarioCallMe).asJava)
-    bypassShortNameCache
+    bypassShortNameCache()
     assertResult(List("arg1", "arg2")) {
       val ref = decisionInput.getReference
       ref.getVariants
@@ -110,7 +107,7 @@ class DecisionTableSuite extends PsiSuite {
   }
 
   /* Call this in case the PsiShortNameCache is hit, but no results are expected. Defaults to empty array */
-  def bypassShortNameCache: Unit = {
+  def bypassShortNameCache(): Unit = {
     when(myPsiShortNamesCache.getClassesByName(any[String], any[GlobalSearchScope])).thenReturn(Array[PsiClass]())
   }
 }
