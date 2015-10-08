2c16bf40255f7eb54b412972bdcf20fc0cfc48f4
Arjan Molenaar <gaphor@gmail.com>
2015-10-08 22:00:53 +0200

    Fix possible NPE in method reference

--- src/main/scala/fitnesse/idea/fixturemethod/MethodOrScenarioArgumentReference.scala
+++ src/main/scala/fitnesse/idea/fixturemethod/MethodOrScenarioArgumentReference.scala
@@ -9,9 +9,12 @@ class MethodOrScenarioArgumentReference(referer: FixtureMethod) extends MethodRe
   override def getVariants = referer.fixtureClass match {
     case Some(fixtureClass) =>
       // FixtureClassReference knows when scenario's can be used along with (Java) fixtures.
-      fixtureClass.getReference.resolve match {
-        case c: PsiClass => c.getAllMethods.map(m => Regracer.regrace(m.getName))
-        case s: ScenarioName => s.arguments.toArray
+      Option(fixtureClass.getReference) match {
+        case Some(ref) => ref.resolve match {
+          case c: PsiClass => c.getAllMethods.map(m => Regracer.regrace(m.getName))
+          case s: ScenarioName => s.arguments.toArray
+        }
+        case None => Array.emptyObjectArray
       }
     case None => Array.emptyObjectArray
   }
