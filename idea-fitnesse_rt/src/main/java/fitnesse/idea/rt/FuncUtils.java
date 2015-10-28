74c474d7f6d65e43e1e04264d5d5e2e986d2f242
Arjan Molenaar <gaphor@gmail.com>
2015-10-28 22:23:53 +0100

    Remove unused imports.

--- idea-fitnesse_rt/src/main/java/fitnesse/idea/rt/FuncUtils.java
+++ idea-fitnesse_rt/src/main/java/fitnesse/idea/rt/FuncUtils.java
@@ -1,7 +1,6 @@
 package fitnesse.idea.rt;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import static java.util.Collections.max;
--- idea-fitnesse_rt/src/main/java/fitnesse/idea/rt/IntelliJFormatter.java
+++ idea-fitnesse_rt/src/main/java/fitnesse/idea/rt/IntelliJFormatter.java
@@ -23,7 +23,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
