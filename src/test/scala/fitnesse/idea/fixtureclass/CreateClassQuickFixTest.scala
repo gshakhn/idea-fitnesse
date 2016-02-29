2bff743708994363c42489910d261c23a536a564
Arjan Molenaar <gaphor@gmail.com>
2016-02-29 23:27:47 +0100

    Fix Quick fix test for IJ15.

--- src/test/scala/fitnesse/idea/fixtureclass/CreateClassQuickFixTest.scala
+++ src/test/scala/fitnesse/idea/fixtureclass/CreateClassQuickFixTest.scala
@@ -1,15 +1,23 @@
 package fitnesse.idea.fixtureclass
 
-import com.intellij.psi.SmartPointerManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Segment
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.{PsiElement, PsiFile, SmartPsiElementPointer, SmartPointerManager}
 import com.intellij.psi.impl.smartPointers.SmartPointerManagerImpl
 import fitnesse.idea.psi.PsiSuite
+import org.mockito.Mockito._
 
 class CreateClassQuickFixTest extends PsiSuite {
 
+  val smartPointerManager = mock[SmartPointerManager]
 
   test("quick fix is available") {
     val table = createTable("| foo bar |")
-    val quickFix = new CreateClassQuickFix(table.fixtureClass.get)
+    val fixtureClass = table.fixtureClass.get
+    when(smartPointerManager.createSmartPsiElementPointer(fixtureClass)).thenReturn(getFixtureClassPointer(fixtureClass))
+
+    val quickFix = new CreateClassQuickFix(fixtureClass)
 
     assertResult(false) {
       // because the mock isInProject implementation returns False
@@ -19,6 +27,22 @@ class CreateClassQuickFixTest extends PsiSuite {
 
   override protected def beforeAll(): Unit = {
     super.beforeAll()
-    myProject.getPicoContainer.registerComponentInstance(classOf[SmartPointerManager].getName, new SmartPointerManagerImpl(myProject))
+    myProject.getPicoContainer.registerComponentInstance(classOf[SmartPointerManager].getName, smartPointerManager)
+  }
+
+  def getFixtureClassPointer(fixtureClass: FixtureClass): SmartPsiElementPointer[FixtureClass] = {
+    new SmartPsiElementPointer[FixtureClass]() {
+      override def getContainingFile: PsiFile = ???
+
+      override def getRange: Segment = ???
+
+      override def getElement: FixtureClass = fixtureClass
+
+      override def getProject: Project = ???
+
+      override def getVirtualFile: VirtualFile = ???
+
+      override def getPsiRange: Segment = ???
+    }
   }
 }
