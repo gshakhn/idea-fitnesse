9112516f553afb637e7796649428aa5f73940977
Arjan Molenaar <gaphor@gmail.com>
2016-09-05 22:31:13 +0200

    Add link resolution to .wiki files

--- src/main/scala/fitnesse/idea/wikilink/WikiLinkReference.scala
+++ src/main/scala/fitnesse/idea/wikilink/WikiLinkReference.scala
@@ -1,33 +1,42 @@
 package fitnesse.idea.wikilink
 
 import com.intellij.openapi.util.TextRange
-import com.intellij.psi.{PsiDirectory, PsiElement, PsiReferenceBase}
+import com.intellij.psi.{PsiDirectory, PsiElement, PsiFile, PsiReferenceBase}
 import fitnesse.idea.filetype.FitnesseFileType
+import fitnesse.wiki.fs.WikiFilePage
 
 class WikiLinkReference(element: WikiLink) extends PsiReferenceBase[WikiLink](element, new TextRange(0, element.getTextLength)) {
+
   override def resolve() : PsiElement = {
     val linkText = element.getText
-
-    val dirs = linkText.split('.')
-    val parentDir: PsiDirectory = getParentDir(dirs)
-    val targetDir = dirs.foldLeft(parentDir) {
-        (currentDir: PsiDirectory, childName: String) => {
-          currentDir match {
-            case null => null
-            case dir: PsiDirectory => dir.findSubdirectory(childName)
-          }
-        }
-      }
-
-    targetDir match {
-      case null => null
-      case _ => targetDir.findFile(FitnesseFileType.CONTENT_TXT_NAME)
-    }
-  }
-
-  private def getParentDir(pagePath: Array[String]): PsiDirectory = {
-    element.getContainingFile.getParent.getParent // go up twice since the immediate parent only contains content.txt and properties.xml
+    val path = linkText.split('.')
+    resolvePathReference(path).orNull
   }
 
   override def getVariants = Array()
+
+  def resolvePathReference(path: Seq[String]): Option[PsiFile] = {
+    val finalPage: String = path.last
+    val targetDir = path.view.dropRight(1).foldLeft(getParentDir)(findSubdirectory)
+
+    findFile(targetDir, finalPage + WikiFilePage.FILE_EXTENSION)
+      .orElse(
+        findFile(findSubdirectory(targetDir, finalPage), FitnesseFileType.CONTENT_TXT_NAME))
+  }
+
+  def findSubdirectory(dir: Option[PsiDirectory], childName: String): Option[PsiDirectory] =
+    dir.flatMap(dir => Option(dir.findSubdirectory(childName)))
+
+  def findFile(dir: Option[PsiDirectory], childName: String): Option[PsiFile] =
+    dir.flatMap(dir => Option(dir.findFile(childName)))
+
+  def getParentDir: Option[PsiDirectory] = {
+    val wikiFile = element.getContainingFile
+    if (wikiFile.getName.endsWith(WikiFilePage.FILE_EXTENSION)) {
+      Option(wikiFile.getParent)
+    } else {
+      // Go up twice since the immediate parent only contains content.txt and properties.xml
+      Option(wikiFile.getParent.getParent)
+    }
+  }
 }
