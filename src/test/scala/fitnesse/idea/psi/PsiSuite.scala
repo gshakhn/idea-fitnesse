572555303e4e4224ca384a11655b868369b622d5
Arjan Molenaar <gaphor@gmail.com>
2017-10-09 23:29:57 +0200

    Update PsiSuite with new API

--- src/test/scala/fitnesse/idea/psi/PsiSuite.scala
+++ src/test/scala/fitnesse/idea/psi/PsiSuite.scala
@@ -64,7 +64,7 @@ trait PsiSuite extends ParserSuite with MockitoSugar {
     assert(fileNode.isParsed)
     val indexFile: File = File.createTempFile("idea", "fitnesse")
     val persistentStringEnumerator = new PersistentStringEnumerator(indexFile)
-    val stubSerializationHelper = new StubSerializationHelper(persistentStringEnumerator)
+    val stubSerializationHelper = new StubSerializationHelper(persistentStringEnumerator, myProject)
 
     stubSerializationHelper.assignId(PsiFileStubImpl.TYPE)
     stubSerializationHelper.assignId(FitnesseElementType.DECISION_INPUT)
