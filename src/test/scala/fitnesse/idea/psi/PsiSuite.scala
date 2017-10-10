8497ec577d9ba97e90a107099fbfe37331511b2a
Arjan Molenaar <gaphor@gmail.com>
2017-10-10 09:39:24 +0200

    Revert "Update PsiSuite with new API"
    This reverts commit 572555303e4e4224ca384a11655b868369b622d5.

--- src/test/scala/fitnesse/idea/psi/PsiSuite.scala
+++ src/test/scala/fitnesse/idea/psi/PsiSuite.scala
@@ -64,7 +64,7 @@ trait PsiSuite extends ParserSuite with MockitoSugar {
     assert(fileNode.isParsed)
     val indexFile: File = File.createTempFile("idea", "fitnesse")
     val persistentStringEnumerator = new PersistentStringEnumerator(indexFile)
-    val stubSerializationHelper = new StubSerializationHelper(persistentStringEnumerator, myProject)
+    val stubSerializationHelper = new StubSerializationHelper(persistentStringEnumerator)
 
     stubSerializationHelper.assignId(PsiFileStubImpl.TYPE)
     stubSerializationHelper.assignId(FitnesseElementType.DECISION_INPUT)
